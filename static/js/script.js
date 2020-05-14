0679b0ec58baff3c7340ed83c3560f1a9e9d48db
Mike <mmenendez@csumb.edu>
2020-05-13 20:01:49 -0700

    Updated credits text

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     $(".credits").on('click', () => {
         Swal.fire({
             title: "Built In A Night",
-            text: "Built and hosted for you by the Interview High team"
+            text: "Built and hosted for you by part of the Interview High team. Carlos Estrada, Jesus Bernal and Mike Menendez"
         })
     })
 })
